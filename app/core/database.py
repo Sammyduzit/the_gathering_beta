efd2fa7170d18ee3fd861bd047f9502ef240edaa
Sammyduzit <sammy.duzit999@gmail.com>
2025-07-05 04:14:46 +0200

    Remove unused imports

--- app/core/database.py
+++ app/core/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError, OperationalError
 from sqlalchemy.orm import declarative_base, sessionmaker
 
